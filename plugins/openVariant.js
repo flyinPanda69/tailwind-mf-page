c7942fc43b780864c53abfcf1ac16cd11fd37f47
Rahul Anand <rarockin002@gmail.com>
2024-12-29 20:54:56 +0530

    Building the navbar: Animate mobile menu icon & create custom variant plugin with "addVariant"

---
+++ plugins/openVariant.js
@@ -0,0 +1,8 @@
+const plugin = require("tailwindcss/plugin");
+
+const openVariant = plugin(function ({ addVariant, e }) {
+  addVariant("group-open", ":merge(.group).open &");
+  });
+
+
+module.exports = openVariant;
